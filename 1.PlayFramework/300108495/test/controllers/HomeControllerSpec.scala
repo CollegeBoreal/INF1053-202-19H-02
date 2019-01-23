bb7925c6b7f5a2094b29b50dd2b47509d6814a36
safaa <safaazaoui@gmail.com>
2019-01-23 11:16:15 -0500

    safaa

--- 1.PlayFramework/300108495/test/controllers/HomeControllerSpec.scala
+++ 1.PlayFramework/300108495/test/controllers/HomeControllerSpec.scala
@@ -1,5 +1,7 @@
 package controllers
 
+
+import akka.http.scaladsl.model.HttpHeader.ParsingResult.Ok
 import org.scalatestplus.play._
 import org.scalatestplus.play.guice._
 import play.api.test._
@@ -21,7 +23,7 @@ class HomeControllerSpec extends PlaySpec with GuiceOneAppPerTest with Injecting
 
       status(home) mustBe OK
       contentType(home) mustBe Some("text/html")
-      contentAsString(home) must include ("Welcome to Play")
+      contentAsString(home) must include ("Hello Boreal!")
     }
 
     "render the index page from the application" in {
@@ -30,7 +32,7 @@ class HomeControllerSpec extends PlaySpec with GuiceOneAppPerTest with Injecting
 
       status(home) mustBe OK
       contentType(home) mustBe Some("text/html")
-      contentAsString(home) must include ("Welcome to Play")
+      contentAsString(home) must include ("Hello Boreal!")
     }
 
     "render the index page from the router" in {
@@ -39,7 +41,23 @@ class HomeControllerSpec extends PlaySpec with GuiceOneAppPerTest with Injecting
 
       status(home) mustBe OK
       contentType(home) mustBe Some("text/html")
-      contentAsString(home) must include ("Welcome to Play")
+      contentAsString(home) must include ("Hello Boreal!")
+    }
+    "afficher le API hello du routage" in {
+      val request = FakeRequest(GET, "/hello/name=safaa")
+      val hello = route(app, request).get
+
+      status(hello) mustBe OK
+      contentType(hello) mustBe Some("text/plain")
+      contentAsString(hello) must include ("name=safaa")
+    }
+    "afficher le API json du routage" in {
+      val request = FakeRequest(GET, "/json")
+      val json = route(app, request).get
+
+      status(json) mustBe  UNSUPPORTED_MEDIA_TYPE
+      contentType(json) mustBe Some("text/html")
+      contentAsString(json) must include("")
     }
   }
 }
