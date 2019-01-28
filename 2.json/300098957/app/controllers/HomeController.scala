82c56ae757d06f559827d24ddb887b65ccb80cd0
setrar <setrar@valiha.com>
2019-01-28 12:04:08 -0500

    :penguin:

--- 2.json/300098957/app/controllers/HomeController.scala
+++ 2.json/300098957/app/controllers/HomeController.scala
@@ -2,7 +2,10 @@ package controllers
 
 import javax.inject._
 import play.api._
+import play.api.libs.json.{JsPath, Writes}
 import play.api.mvc._
+import play.api.libs.functional.syntax._
+import play.libs.Json
 
 /**
  * This controller creates an `Action` to handle HTTP requests to the
@@ -25,4 +28,44 @@ class HomeController @Inject()(cc: ControllerComponents) extends AbstractControl
   def mapage() = Action { request =>
     Ok(views.html.mapage())
   }
+
+  case class Location(lat: Double, long: Double)
+
+  case class Place(name: String, location: Location)
+
+  object Place {
+
+    var list: List[Place] = {
+      List(
+        Place(
+          "Sandleford",
+          Location(51.377797, -1.318965)
+        ),
+        Place(
+          "Watership Down",
+          Location(51.235685, -1.309197)
+        )
+      )
+    }
+
+    def save(place: Place) = {
+      list = list ::: List(place)
+    }
+  }
+
+  implicit val locationWrites: Writes[Location] = (
+    (JsPath \ "lat").write[Double] and
+      (JsPath \ "long").write[Double]
+    )(unlift(Location.unapply))
+
+  implicit val placeWrites: Writes[Place] = (
+    (JsPath \ "name").write[String] and
+      (JsPath \ "location").write[Location]
+    )(unlift(Place.unapply))
+
+  def listPlaces = Action {
+    val json = Json.toJson(Place.list)
+    Ok(json)
+  }
+
 }
