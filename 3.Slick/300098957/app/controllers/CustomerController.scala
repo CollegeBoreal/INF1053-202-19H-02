3d3d811ecdf5bf64a57f40150e964cd275ea412d
setrar <setrar@valiha.com>
2019-01-30 11:53:17 -0500

    .

---
+++ 3.Slick/300098957/app/controllers/CustomerController.scala
@@ -0,0 +1,12 @@
+package controllers
+
+import javax.inject.{Inject, Singleton}
+import play.api.mvc.{AbstractController, ControllerComponents}
+
+@Singleton()
+class CustomerController @Inject() (cc: ControllerComponents)
+  extends AbstractController(cc) {
+
+
+
+}
