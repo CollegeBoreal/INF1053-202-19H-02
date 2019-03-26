ecb63ed3f5fc2e5b726bcfa42314be56e13a3b5c
Setra <setrar@valiha.com>
2019-03-26 17:28:53 -0400

    .

--- 4.Monad/playslickapp/conf/evolutions/default/00004_insert_logins.sql
+++
@@ -1,8 +0,0 @@
-# Sample data for logins schema
-
-# --- !Ups
-
-INSERT INTO LOGINS (login, providerId, providerKey)
-VALUES
-# --- !Downs
-
--- 4.Monad/playslickapp/conf/evolutions/default/00008_insert_artists.sql
+++
@@ -1,8 +0,0 @@
-# Sample data for artists schema
-
-# --- !Ups
-
-INSERT INTO ARTISTS ()
-VALUES ()
-# --- !Downs
-
