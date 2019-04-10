4b8bdf2bfb9fc20c78e845382481bb7945132e81
ameliedubois <fichou@hotmail.com>
2019-04-10 11:07:57 -0400

    Amelie

---
+++ 4.Monad/300089781/app/controllers/ProfileController.scala
@@ -0,0 +1,21 @@
+package controllers
+
+import dao.ProfileDao
+import javax.inject.{Inject, Singleton}
+import models.{ Profile}
+import play.api.libs.json.{Format, Json}
+import play.api.mvc.{AbstractController, Action, AnyContent, ControllerComponents}
+
+import scala.concurrent.ExecutionContext
+
+@Singleton()
+class ProfileController @Inject()(cc: ControllerComponents, profileDao: ProfileDao)(
+  implicit ec: ExecutionContext)
+  extends AbstractController(cc) {
+
+  implicit val fmt: Format[Profile] = Json.format[Profile]
+
+  def getAll: Action[AnyContent] = Action.async { implicit request =>
+    for { profile <- profileDao.getAll } yield Ok(Json.toJson(profile))
+  }
+}
--- 4.Monad/300089781/app/models/Models.scala
+++ 4.Monad/300089781/app/models/Models.scala
@@ -13,4 +13,4 @@ case class Band(band: Option[Int], name: String, description: String)
 
 case class Member(band: Option[Int], artist: String)
 
-case class Artist()
+case class Profile(profile: Option[Int], name: String, lastname: String, address: String, street: String, city: String, state: String, zip: String)
